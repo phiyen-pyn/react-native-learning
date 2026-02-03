8d3d9f6bbad30b98a0fcc922d4f4a1dbfd525a34
phiyen-pyn <phiyen.100304@gmail.com>
2026-02-03 14:21:55 +0700

    Adding ripple effect

--- track-goal/components/GoalItem.js
+++ track-goal/components/GoalItem.js
@@ -1,9 +1,26 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable, Platform } from "react-native";
 
 function GoalItem(props) {
+  /* We have two ways to pass the id to GoalItem component for deletion:
+  1. Have a separate function in this GoalItem class just like the GoalInput has the addGoalHandler function to call props.onDeleteItem with the id by use a wrapper function or arrow function.
+  Example:
+    function deleteItemHandler() {
+      props.onDeleteItem(props.id);
+    }
+    Then use onPress={deleteItemHandler} in the Pressable component.  
+  2. Use bind method to bind the id to the onPress event handler directly in the JSX code.
+  Example:
+    onPress={props.onDeleteItem.bind(this, props.id)}
+  Here we are using the second approach. */
+  /* bind is a basic JavaScript function that allows you to preconfigure a function for future execution. */
+  /* The id which is gotten through the props is sent as a parameter value to the function that we receive on the props.onDeleteItem. which is the deleteGoalHandler function in App.js */
   return (
     <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
+      <Pressable onPress={props.onDeleteItem.bind(this, props.id)}
+        android_ripple={{ color: '#210644' }}
+        style={({ pressed }) => pressed && Platform.OS === 'ios' && styles.pressedItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -12,10 +29,15 @@ export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
-    padding: 8,
     marginVertical: 4,
     backgroundColor: "#e4e4e4",
     borderRadius: 4,
-    fontSize: 16,
+    overflow: 'hidden'
   },
+  goalText: {
+    padding: 8
+  },
+  pressedItem: {
+    opacity: 0.5
+  }
 });
